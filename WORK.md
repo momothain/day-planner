c9f8841cf36549acf3f514801f9280c4c0248f57
Morgann Thain <mthain@cs.brown.edu>
2025-12-02 09:42:28 -0500

    ex updates, fmting. dur: factory->computed field property
    old updates commited

--- WORK.md
+++ WORK.md
@@ -1,4 +1,18 @@
 # log
+## 10/7/25
+- fixed the errors. Actually, Task was erroring because of the `kind alias="type"` causing the code's sets `kind = "xxx"` not registering
+    - solved with `model_config = dict(populate_by_name=True)`
+- settled on `TimeUnion` as the usable type, and Time just exists for shared functionality
+    - this means people that use TimeUnion have to handle each subtype's fields and functionality separately, as there is not interface defined for how to interact with all Time implementations, and the subclasses thus don't have e.g. `get_time_representation` or `set_time` or other stuff
+- TODO: change prints to logs, and tests to DEBUG level
+- edit, export functions. but to edit non-visually?....
+- TODO:-next find a way to make Visuals then Interaction (so a UI)...MVP streamlit/dash/NiceGUI? or actual html-react
+
+### Plan's time invariants
+- [x] 24hours in a day max (handled by MinSinceMidnight maxval = 1439)
+- [] non-overlap time
+- [] order tasks by time
+
 ## 10/2/25
 ### actual 
 - a lot of [field_validator](https://docs.pydantic.dev/2.11/concepts/validators/#field-validators) work to do the reading and preprocessing of all fields. "  task name " -> "Task Name", "t1;t2" -> ls
--- models.py
+++ models.py
@@ -1,47 +1,66 @@
+# model_fmt.py
 import json
 import re
 from typing import List, Literal, Annotated, Optional, Self, Union
-from pydantic import BaseModel, Field, field_validator, model_validator
+from pydantic import BaseModel, Field, computed_field, field_validator, model_validator
 from pathlib import Path
 import csv
 
 """Time is minutes-since-midnight. Date, timezone in Plan."""
 
-MinSinceMidnight = Annotated[int, Field(ge=0,le=1439)] # 24hr * 60min/hr = 1440. 23:59=1439min
+MinSinceMidnight = Annotated[
+    int, Field(ge=0, le=1439)
+]  # 24hr * 60min/hr = 1440. 23:59=1439min
 
-class TimeBlock(BaseModel):
+class Time(BaseModel):
+    model_config = dict(populate_by_name=True)
+    kind: Literal["block", "duration"] = Field(alias="type", repr=False)
+    
+class TimeBlock(Time):
     kind: Literal["block"] = "block"
     start: MinSinceMidnight
-    end: MinSinceMidnight #val > start below
-    duration: MinSinceMidnight = Field(default_factory=lambda data: data["end"] - data["start"])
+    end: MinSinceMidnight  # val > start below
+    
+    @computed_field
+    @property
+    def duration(self) -> MinSinceMidnight:
+        self.end - self.start
+    # duration: MinSinceMidnight = Field(
+    #     default_factory=lambda data: data["end"] - data["start"]
+    # )
 
     #
-    @model_validator(mode='after')
-    def end_over_start(self) -> Self:
+    @model_validator(mode="after")
+    def _end_over_start(self) -> Self:
         if not (self.end > self.start):
-            raise ValueError(f"End ({self.end}) must be greater than start ({self.start}).")
-        return Self
-    
-class TimeDuration(BaseModel):
+            raise ValueError(
+                f"End ({self.end}) must be greater than start ({self.start})."
+            )
+        return self
+
+
+class TimeDuration(Time):
     kind: Literal["duration"] = "duration"
     duration: MinSinceMidnight
-
-# class Time(BaseModel):
-#     kind: Literal["block", "duration"] = Field(alias="type", repr="False")
-Time = Union[TimeBlock | TimeDuration]
-#TODO: handle multi-type properly
+TimeUnion = Annotated[Union[TimeBlock | TimeDuration], Field(alias="Time", discriminator="kind")]
 
 ###
 """"""
 class Task(BaseModel):
+    # Settings
+    model_config = dict(populate_by_name=True)
     # Core
-    name: str = Field(min_length=1, max_length=64, alias="Task") # DB #store as lower, rep as Pascal?
-    time: Time = Field(discriminator="kind", alias="Time")
+    name: str = Field(
+        min_length=1, max_length=64, alias="Task"
+    )  # DB #store as lower, rep as Pascal?
+    time: TimeUnion # Have to use union can't use parent class :(
     #
-    tags: List[str] = Field(alias="Tags") # DB
-    freq: str = None # num / period # used for checking once time spent statistics are aggregated
-    subtasks: Optional[List[str]] = None  #in a DB
-    
+    tags: List[str] = Field(default=[], alias="Tags"
+                            )  # DB
+    freq: str = None  # num / period # used for checking once time spent statistics are aggregated
+    subtasks: Optional[List[str]] = None  # in a DB
+    details: Optional[dict] = None
+
     ###
 
     ## Preprocess, Read Validate
@@ -49,7 +68,7 @@ class Task(BaseModel):
     @classmethod
     def preprocess_name(cls, name: str) -> str:
         s = name.strip().lower()
-        return ' '.join((word.capitalize()) for word in s.split(' '))
+        return " ".join((word.capitalize()) for word in s.split(" "))
 
     @field_validator("tags", "subtasks", mode="before")
     def preprocess_list(cls, ls: str | list) -> list:
@@ -63,47 +82,85 @@ class Task(BaseModel):
             raise TypeError(f"require str or list, got {ls}")
 
     @field_validator("time", mode="before")
-    #read_time
+    # read_time
     def preprocess_time(cls, t) -> Time:
-        if isinstance(t, str):
+        if isinstance(t, Time):
+            return t
+        elif isinstance(t, str):
             t = t.strip()
             if re.match(r"\d\d:\d\d[-,]\d\d:\d\d", t):
-                return TimeBlock(kind='block', 
-                                    start=int(t[0:2])*60 + int(t[3:5]), 
-                                    end=int(t[0+6:2+6])*60 + int(t[3+6:5+6]))
+                return TimeBlock(
+                    kind="block",
+                    start=int(t[0:2]) * 60 + int(t[3:5]),
+                    end=int(t[0 + 6 : 2 + 6]) * 60 + int(t[3 + 6 : 5 + 6]),
+                )
             elif re.match(r"\d+", t):
-                return TimeDuration(kind='duration',duration=int(t))
+                return TimeDuration(kind="duration", duration=int(t))
             # elif re.match(r"\d+[-,]\d+", t):
             #     ... #return TimeDurationRange()
-        #TODO: what's going on here? why isn't this reached?
-        print(0 / 0 )
-        print("eGOTHERE")
-        raise ValueError(f"Couldn't read {t}. Please use 'xx:xx-xx:xx', duration, 'xx-xx' duration range")
-    
+            
+        raise ValueError(
+            f"Couldn't read {t}. Please use 'xx:xx-xx:xx', duration, 'xx-xx' duration range"
+        )
         
+    ## Overrides
+    def __str__(self):
+        return self.model_dump_json(indent=2, exclude_none=True)
+
+
 def test_task():
-    p1 = Task(name="task1", time=TimeDuration(kind="duration", duration=60), tags=["tg1","tg2"], freq="5/wk")
+    print("START test_task(): ===============")
+    p1 = Task(
+        name="task1",
+        time=TimeDuration(kind="duration", duration=60),
+        tags=["tg1", "tg2"],
+        freq="5/wk",
+    )
     print(p1)
-    p2 = Task(name=" task2   ", time=TimeDuration(kind="duration", duration=60), tags=" tg1 ;tg2", freq="2/day")
+    
+    p2 = Task(
+        name=" task2   ",
+        time=TimeDuration(kind="duration", duration=60),
+        tags=" tg1 ;tg2",
+        freq="2/day",
+    )
     print(p2)
+    
+    p3 = Task(
+        name="sleep",
+        time=TimeBlock(kind="block", start=0, end=480),
+        tags="core",
+        freq="1/day",
+    )
+    print(p3)
+    print("END test_task(): ===============")
+    print()
+    
     return
+
 
 ###
 """"""
 class Plan(BaseModel):
     tasks: List[Task]
-    timezone: str = None # type/val?
-    date: str = None # type # means its instantiated
-    start_time: MinSinceMidnight = 0 # aka "midnight" for our minutes-since-midnight time representation
+    timezone: str = None  # type/val?
+    date: str = None  # type # means its instantiated
+    start_time: MinSinceMidnight = (
+        0  # aka "midnight" for our minutes-since-midnight time representation
+    )
 
-    ## time invar
-    # invariant -- 24hours in a day max
-    # inv - non-overlap time
+    ## time invariants
+    # [x] 24hours in a day max (handled by MinSinceMidnight maxval = 1439)
+    # [] non-overlap time
+    # [] order tasks by time
+    
+    ## QoL
+    # - visualize tasks by time spatially
 
     @classmethod
     def read_csv(cls, fp: Path) -> Self:
         print(f"reading Plan from {fp}")
-        with fp.open(mode='r') as f:
+        with fp.open(mode="r") as f:
             reader = csv.DictReader(f)
             tasks = [Task.model_validate(row) for row in reader]
             # tasks = []
@@ -111,15 +168,28 @@ class Plan(BaseModel):
             #     print(row)
             #     Task.model_validate(row)
             return Plan(tasks=tasks)
+        
     
+    ## Overrides
+    def __str__(self):
+        return self.model_dump_json(indent=2, exclude_none=True)
+
+
 def test_plan():
+    print("START test_plan(): ===============")
+    
     ex = Path("./data/schedule-example.csv")
     p1 = Plan.read_csv(ex)
-    print(json.dumps(p1.model_dump_json(), indent=2))
-
+    print(p1.model_dump_json(indent=2))
+    
+    print("END test_plan(): ===============")
+    print()
+    return
 
 ### Features
 
+### Run Test
 if __name__ == "__main__":
-    # test_task()
+    # t: Time = 
+    test_task()
     test_plan()
