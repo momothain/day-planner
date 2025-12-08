80d01ddfd037cb3a1d806781af3b9a6e092a95a6
Morgann Thain <momothain.cs@gmail.com>
2025-12-08 14:43:53 -0500

    write aggs to csv

--- aggregation.py
+++ aggregation.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from config import Config
 from models import Plan, Task
 
@@ -14,21 +15,25 @@ def plan_to_df(plan: Plan):
 
     return df
 
-def agg_duration_by_task(plan_df: pd.DataFrame):
+def agg_duration_by_task(plan_df: pd.DataFrame, out_fp: Path=Config.output / "dur_by_task.log.csv"):
     task_groups = plan_df.groupby("name")
     task_sums = task_groups["duration"].sum()
     task_sums = task_sums.sort_values(ascending=False)
+    task_sums.to_csv(out_fp)
     print(task_sums)
 
-def agg_duration_by_tags(plan_df: pd.DataFrame):
+def agg_duration_by_tags(plan_df: pd.DataFrame, out_fp: Path=Config.output / "dur_by_tags.log.csv"):
     dupe_df = plan_df.explode("tags")
-    print(dupe_df.groupby("tags")["duration"].sum().sort_values(ascending=False))
+    sums = dupe_df.groupby("tags")["duration"].sum().sort_values(ascending=False)
+    sums.to_csv(out_fp)
+    print(sums)
+    
     
 if __name__ == "__main__":
     # p = Plan(tasks=[], start_time=1)
     p = Plan.read_csv(Config.plan_example)
     df = plan_to_df(p)
     agg_duration_by_task(df)
-    # agg_duration_by_tags(df)
+    agg_duration_by_tags(df)
     # print(df.head())
     print("done agg")
--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config:
     ### FILEPATHS
     test_output = Path("./test_output")
     test_output.mkdir(exist_ok=True)
+    output = Path("./output")
+    output.mkdir(exist_ok=True)
     
     plan_example = Path("./data/schedule-example.csv")
     dayplan_example = Path("./data/example1.dayplan")
